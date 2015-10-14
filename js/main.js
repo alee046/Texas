f9b5f9567bacdd955a935e18aeb7c8f8724fe437
Anthony Lee <alee046@ucr.edu>
2015-10-13 18:13:25 -0700

    changed cards to objects, changed A>14

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 console.log("js loaded");
+
 ///Globals
-var ranks = new Array ("01", "02", "03", "04", "05", "06","07","08","09","10","11","12","13");
+var ranks = new Array ("02", "03", "04", "05", "06","07","08","09","10","11","12","13","14");
 var suits = new Array ("c", "d", "h", "s");
-var deck = new Array ();
+var deck = [];
 var playerOne = [];
-var playerTwo = [];
+var playerTwo = [];/// make object
+var rankEval = [];
+var suiteEval = [];
 var board = [];
 var burnBoard = [];
 var playerOneBank = 100000;
@@ -13,6 +16,7 @@ var tPot = 0;
 var rPot= 0;
 var player = 1;
 var turnCount = 0;
+var h
 ///Bet functions
 function playerTurn(){
 	if(player===1){
@@ -38,12 +42,12 @@ function makeBet(){
 	
 	if (player===1){
 		playerOneBank= playerOneBank-parseInt($("#bet-input").val(),10);
-		$('#playerOneBankVal').val(playerOneBank);
-		rPot=rPot+ parseInt($("#bet-input").val(),10);
-		$('#roundPotValue').val(rPot);
-		tPot=tPot+ parseInt($("#bet-input").val(),10);
-		$('#totalPotValue').val(tPot);
-		playerTurn();
+			$('#playerOneBankVal').val(playerOneBank);
+			rPot=rPot+ parseInt($("#bet-input").val(),10);
+			$('#roundPotValue').val(rPot);
+			tPot=tPot+ parseInt($("#bet-input").val(),10);
+			$('#totalPotValue').val(tPot);
+			playerTurn();
 
 		}else{
 		
@@ -93,7 +97,11 @@ function makeBank(){
 	$('#playerOneBankVal').val(playerOneBank);
 	$('#playerTwoBankVal').val(playerTwoBank);
 }
+
+
 ///Deck functions
+
+
 function makeDeck(m){
 
 	var j, k;
@@ -101,7 +109,10 @@ function makeDeck(m){
   	m = ranks.length * suits.length;
 		for (j = 0; j< suits.length; j++)
 			for (k = 0; k<ranks.length; k++){
-				var newDeck= deck.push(suits[j]+ranks[k])
+				var newDeck= deck.push({
+					suit:suits[j],
+					rank:ranks[k]
+				})
 			
 		
 	};
@@ -127,13 +138,13 @@ function dealHands( ) {
 
   if (deck.length > 0){
     playerOne.push(deck.shift());
-    $("#playerOneHand").append('<img src=assets/' + playerOne[0] + ".png>");
+    $("#playerOneHand").append('<img src=assets/' + playerOne[0].suit+playerOne[0].rank + ".png>");
     playerTwo.push(deck.shift());
-    $("#playerTwoHand").append('<img src=assets/' + playerTwo[0] + ".png>");
+    $("#playerTwoHand").append('<img src=assets/' + playerTwo[0].suit+playerTwo[0].rank + ".png>");
     playerOne.push(deck.shift());
-    $("#playerOneHand").append('<img src=assets/' + playerOne[1] + ".png>");
+    $("#playerOneHand").append('<img src=assets/' + playerOne[1].suit+playerOne[1].rank + ".png>");
     playerTwo.push(deck.shift());
-    $("#playerTwoHand").append('<img src=assets/' + playerTwo[1] + ".png>");
+    $("#playerTwoHand").append('<img src=assets/' + playerTwo[1].suit+playerTwo[1].rank + ".png>");
  	} else
     return null;
 }
@@ -141,13 +152,13 @@ function dealHands( ) {
 function dealFlop(){
 	if (deck.length > 0){
 	burnBoard.push(deck.shift());
-	$("#burnBoardDisp").append('<img src=assets/' + burnBoard[0] + ".png>");
+	$("#burnBoardDisp").append('<img src=assets/' + burnBoard[0].suit+burnBoard[0].rank + ".png>");
 	board.push(deck.shift());
-	$("#boardDisp").append('<img src=assets/' + board[0] + ".png>");
+	$("#boardDisp").append('<img src=assets/' + board[0].suit+board[0].rank + ".png>");
 	board.push(deck.shift());
-	$("#boardDisp").append('<img src=assets/' + board[1] + ".png>");
+	$("#boardDisp").append('<img src=assets/' + board[1].suit+board[1].rank + ".png>");
 	board.push(deck.shift());
-	$("#boardDisp").append('<img src=assets/' + board[2] + ".png>");
+	$("#boardDisp").append('<img src=assets/' + board[2].suit+board[2].rank + ".png>");
 	} else
 	return null;
 }
@@ -155,21 +166,23 @@ function dealFlop(){
 function dealTurn(){
 	if (deck.length >0){
 		burnBoard.push(deck.shift());
-		$("#burnBoardDisp").append('<img src=assets/' + burnBoard[1] + ".png>");
+		$("#burnBoardDisp").append('<img src=assets/' + burnBoard[1].suit+burnBoard[1].rank + ".png>");
 		board.push(deck.shift());
-		$("#boardDisp").append('<img src=assets/' + board[3] + ".png>");
+		$("#boardDisp").append('<img src=assets/' + board[3].suit+board[3].rank + ".png>");
 	} else
 	return null;
 }
 function dealRiver(){
 	if (deck.length >0){
 		burnBoard.push(deck.shift());
-		$("#burnBoardDisp").append('<img src=assets/' + burnBoard[2] + ".png>");
+		$("#burnBoardDisp").append('<img src=assets/' + burnBoard[2].suit+burnBoard[2].rank + ".png>");
 		board.push(deck.shift());
-		$("#boardDisp").append('<img src=assets/' + board[4] + ".png>");
+		$("#boardDisp").append('<img src=assets/' + board[4].suit+board[4].rank + ".png>");
 	} else
 	return null;
 }
 
-
+function imageReference(){
+	return playerOne[0].suit+playerOne[0].rank
+}
 
