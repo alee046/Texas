8b216f53ae9ef706f17c349137908270ca511308
Anthony Lee <alee046@ucr.edu>
2015-10-15 20:47:09 -0700

    pairs/fh, hand heirarchy

--- js/main.js
+++ js/main.js
@@ -23,7 +23,6 @@ var rPot= 0;//round pot = goes to 0 after each round
 
 var player = 1;//turn function
 var turnCount = 0;//tracks turn count
-var handVal = 0;
 
 //winning logic arrays
 var rankEval = []//sorts 
@@ -38,15 +37,20 @@ var checkID1 = [];//sorts id ---(for royalflush or straightflush)
 var checkRank2 = [];//
 var checkSuit2 = [];//same as above except for player 2
 var checkID2 = [];//
-
-//Pair, Trip values
-var pairCount = 0;
-var tripCount = 0;
+var p1HandScores= [];
+var p2HandScores= [];
 //single High card function//
 var parseRank1 = [];//parse numbers of rank (- the 0)
 var parseRank2 = [];
 var highCard1 = 0;
 var highCard2 = 0;
+
+//pairs-trips-fullhouse//
+var pairs1=0;
+var pairs2=0;
+var trips1=0;
+var trips2=0;
+
 //flush suite count functions
 var hearts1= 0;
 var spades1= 0 ;
@@ -56,58 +60,65 @@ var hearts2= 0;
 var spades2= 0 ;
 var clovers2= 0;
 var diamonds2= 0;
-///Bet functions
 
-function playerTurn(){
+
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+///Bet functions
+function playerTurn(){ ////Determines who's move
 	if(player===1){
+
 		$('#Turn').html("Player Two");
 			player-=1;
 	}else {
+
 		$('#Turn').html("Player One");
 			player+=1;	
 	}
+
 }
 
 function checkMove(){
 	if(isNaN($('#bet-input'))==true){
+
 			rPot=0;
 			$('#roundPotValue').val(rPot);
 			console.log("player checks")
 			playerTurn();
+
 	}else console.log("cant chec");
 
 
 
 }
-function makeBet(){
+
+function makeBet(){ ////Make a bet
 	
 	if (player===1){
-		playerOneBank= playerOneBank-parseInt($("#bet-input").val(),10);
-			$('#playerOneBankVal').val(playerOneBank);
-			rPot=rPot+ parseInt($("#bet-input").val(),10);
-			$('#roundPotValue').val(rPot);
-			tPot=tPot+ parseInt($("#bet-input").val(),10);
-			$('#totalPotValue').val(tPot);
-			playerTurn();
+		var current=playerOneBank;
+		var Icurrent="#playerOneBankVal";
 
-		}else{
-		
-		playerTwoBank= playerTwoBank-parseInt($("#bet-input").val(),10);
-		$('#playerTwoBankVal').val(playerTwoBank);
+	}else{ var current=playerTwoBank;
+		   var Icurrent="#playerTwoBankVal";}
+
+		current= current-parseInt($("#bet-input").val(),10);
+		$(Icurrent).val(current);
 		rPot=rPot+ parseInt($("#bet-input").val(),10);
 		$('#roundPotValue').val(rPot);
 		tPot=tPot+ parseInt($("#bet-input").val(),10);
 		$('#totalPotValue').val(tPot);
-		
 		playerTurn();
 
 		}
 
 	
 
-};
-function makeCall(){
+
+
+function makeCall(){  ///Call the Bet
 	if (player===1){
+
 			playerOneBank=playerOneBank-rPot;
 		$('#playerOneBankVal').val(playerOneBank);
 		rPot=rPot+ parseInt($("#bet-input").val(),10);
@@ -132,7 +143,10 @@ function makeCall(){
 rPot=0;
 $('#roundPotValue').val(0);
 };
-function makeBank(){
+
+
+function makeBank(){  ///Links pot values to Div display
+
 	$('#roundPotValue').val(rPot);
 	$('#totalPotValue').val(tPot);
 	$('#playerOneBankVal').val(playerOneBank);
@@ -140,13 +154,16 @@ function makeBank(){
 }
 
 
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 ///Deck functions
 function deckId(){
+
 	for (i=0; i<=51;i++)
 		deck[i].id=i
 }
 
-function makeDeck(m){
+function makeDeck(m){///create deck
 
 	var j, k;
 	var m;
@@ -157,35 +174,37 @@ function makeDeck(m){
 					suit:suits[j],
 					rank:ranks[k]
 				})
-			
-		
 	};
 	deckId();
 	makeBank();
 }
-function deckId(){
-	for (i=0; i<=51;i++)
-		deck[i].id=i
-}
+
+
+
 function shuffle(n) {
-	n=(Math.random()*100)
+
+  n=(Math.random()*100)
   var i, j, k;
   var temp;
-
   // Shuffle the stack 'n' times.
-
   for (i = 0; i < n; i++)
     for (j = 0; j < deck.length; j++) {
+
       k = Math.floor(Math.random() * deck.length);
       temp = deck[j];
       deck[j] = deck[k];
       deck[k] = temp;
     }
 }
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 //Dirty dealings
+
+
 function dealHands( ) {
 
   if (deck.length > 0){
+
     playerOne.push(deck.shift());
     $("#playerOneHand").append('<img src=assets/' + playerOne[0].suit+playerOne[0].rank + ".png>");
     playerTwo.push(deck.shift());
@@ -194,11 +213,13 @@ function dealHands( ) {
     $("#playerOneHand").append('<img src=assets/' + playerOne[1].suit+playerOne[1].rank + ".png>");
     playerTwo.push(deck.shift());
     $("#playerTwoHand").append('<img src=assets/' + playerTwo[1].suit+playerTwo[1].rank + ".png>");
+
  	} else
     return null;
 }
 
 function dealFlop(){
+
 	if (deck.length > 0){
 	burnBoard.push(deck.shift());
 	$("#burnBoardDisp").append('<img src=assets/backcard.jpg>');
@@ -208,40 +229,48 @@ function dealFlop(){
 	$("#boardDisp").append('<img src=assets/' + board[1].suit+board[1].rank + ".png>");
 	board.push(deck.shift());
 	$("#boardDisp").append('<img src=assets/' + board[2].suit+board[2].rank + ".png>");
-	} else
-	return null;
+	} 
 }
 
 function dealTurn(){
+
 	if (deck.length >0){
 		burnBoard.push(deck.shift());
 		$("#burnBoardDisp").append('<img src=assets/backcard.jpg>');
 		board.push(deck.shift());
 		$("#boardDisp").append('<img src=assets/' + board[3].suit+board[3].rank + ".png>");
-	} else
-	return null;
+	} 
+
 }
 function dealRiver(){
+
 	if (deck.length >0){
 		burnBoard.push(deck.shift());
 		$("#burnBoardDisp").append('<img src=assets/backcard.jpg>');
 		board.push(deck.shift());
 		$("#boardDisp").append('<img src=assets/' + board[4].suit+board[4].rank + ".png>");
-	} else
-	return null;
+	} 
 	handEval();
+	winStack();
+	chickenDinner();
+
 }
 
-
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 ///winconditions
+
+
 function handEval(){//sorts into hand evaluate array
+
 cardEval1=$.merge( $.merge( [], playerOne ), board );
 cardEval2=$.merge( $.merge( [], playerTwo ), board );
 sortHand();
+console.log("checking for winner...")
 }
 
 
-function sortHand(){//automattically sorts into 3 arrays for rank suite and id#
+function sortHand(){//automatically sorts into 3 arrays for rank suite and id#
 
 	for (var i = 0 ; i<cardEval1.length; i++){
 		checkSuit1.push(cardEval1[i].suit);
@@ -258,7 +287,66 @@ function sortHand(){//automattically sorts into 3 arrays for rank suite and id#
         checkID2.sort()
  	}
 }
-function noRepeat(x) {//cardRank no repeats
+
+
+function winStack(){
+	noZero1();
+	noZero2();
+	straight();
+	checkSuits();
+	checkFlush();
+	checkMulti();
+	highestPair();
+	checkSingle();
+
+}
+function checkWinningHands(x){
+	for(i=0;i<=p1HandScores.length;i++){
+		if(x===1){
+			$('#Hands').html("High Card")
+		}
+		if(x===2){
+			$('#Hands').html("pairs");
+		}if (x===3){
+			$('#Hands').html("2 pairs");
+		}if (x===4){
+			$('#Hands').html("Trips");
+		}if (x===5){
+			$('#Hands').html("Straight");
+		}if (x===6){
+			$('#Hands').html("Flush");
+		}if (x===7){
+			$('#Hands').html("Full House");
+		}if (x===8){
+			$('#Hands').html("4 of a Kind");
+		}if (x===9){
+			$('#Hands').html("Straight Flush");
+		}if (x===10){
+			$('#Hands').html("Royal Flush");
+		}
+		}
+	}
+function chickenDinner(){
+
+	var p1Max = Math.max.apply(null,p1HandScores);
+	var p2Max = Math.max.apply(null,p2HandScores);
+
+	if (p1Max > p2Max){
+		$('#Winner').html("player1 Wins");
+		checkWinningHands(p1Max);
+	}
+	else if(Math.max.apply(null,p1HandScores)===Math.max.apply(null,p2HandScores)){
+		$('#Winner').html("split pot");
+		checkWinningHands(p1Max)+checkWinningHands(p2Max)
+	}	
+	else {$('#Winner').html("player2 Wins");
+		checkWinningHands(p2Max);}
+	}
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+///STRAIGHT FUNCTINONS
+function noRepeat(x) {//Remove repeating values in Rank 1+2 array. (for)
+
   var i, l = x.length, result = [];
   for (i = 0; i < l; i++) {
     if (result[result.length - 1] != x[i]) {
@@ -267,29 +355,59 @@ function noRepeat(x) {//cardRank no repeats
   }
   return result;
 }
-function noDupes(){
+function noDupes(){//no repeats for straight function
+
 	noRepeat(checkRank1);
 	noRepeat(checkRank2);
 }
 function straight(){//return if player has straight
 
-	for(i=0; i<=2; i++)
-	if(checkRank2[i] == checkRank2[i+1]-1 && checkRank2[i+1]-1 == checkRank2[i+2]-2&& checkRank2[i+2]-2 == checkRank2[i+3]-3&&	checkRank2[i+3]-3 == checkRank2[i+4]-4)
-		console.log("player2 has straight");
-	else if((checkRank1[i] == checkRank1[i+1]-1 
-		&& checkRank1[i+1]-1 == checkRank1[i+2]-2
-		&& checkRank1[i+2]-2 == checkRank1[i+3]-3
-		&&	checkRank1[i+3]-3 == checkRank1[i+4]-3))
-		console.log("player1 has straight");
-	else console.log("no straight");
+		var reCheck1=noRepeat(parseRank1);
+		var reCheck2=noRepeat(parseRank2);
+			for(i=0; i<=2; i++){
+				if (reCheck1[i] == reCheck1[i+1]-1 
+				&& reCheck1[i+1]-1 == reCheck1[i+2]-2
+				&& reCheck1[i+2]-2 == reCheck1[i+3]-3
+				&& reCheck1[i+3]-3 == reCheck1[i+4]-4){
+				
+				p1HandScores.push(5);
+			}
+			if((reCheck2[i] == reCheck2[i+1]-1 
+					&& reCheck2[i+1]-1 == reCheck2[i+2]-2
+					&& reCheck2[i+2]-2 == reCheck2[i+3]-3
+					&&	reCheck2[i+3]-3 == reCheck2[i+4]-4)){
+					
+				p2HandScores.push(5);
+}
+}
+}
+function straightF(){//straight flushes
+
+	for(i=0; i<=checkID1.length; i++)
+	if (checkID1[i] == checkID1[i+1]-1 
+		&& checkID1[i+1]-1 == checkID1[i+2]-2
+		&& checkID1[i+2]-2 == checkID1[i+3]-3
+		&& checkID1[i+3]-3 == checkID1[i+4]-4){
+
+		p2HandScores.push(9);
+	}else if((checkID2[i] == checkID2[i+1]-1 
+		&& checkID2[i+1]-1 == checkID2[i+2]-2
+		&& checkID2[i+2]-2 == checkID2[i+3]-3
+		&&	checkID2[i+3]-3 == checkID2[i+4]-4)){
+		
+		p1HandScores.push(9);}
 }
 
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 //flush functions
 function flush() {
 	checkSuits();
 	checkFlush();
 }
-function checkSuits(){//sorts suits into different variables
+function checkSuits(){
+//sorts suits into different variables
+
 	for (i=0; i<checkSuit1.length; i++){
 		if (checkSuit1[i] === "c"){
 			clovers1 ++;
@@ -310,31 +428,41 @@ function checkSuits(){//sorts suits into different variables
 		}
 	}
 }
+
 function checkFlush(){//logs who has flush
+
 	if (clovers1>=5 || diamonds1>=5 || hearts1>=5 || diamonds1>=5){
-		console.log("player1 flush");
+	
+		p1HandScores.push(6);
+
 	}if (clovers2>=5 || diamonds2>=5 || hearts2>=5 || diamonds2>=5){
-		console.log("player2 flush");
+	
+		p2HandScores.push(6);
 	}
 }
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 //Single Value functions
 function noZero1(){
+
 	for (i=0; i<checkRank1.length; i++){
 		parseRank1.push(parseInt((checkRank1[i]))) ///////Take out 0s in the values.
 	}
 }
 function noZero2(){
+
 	for (i=0; i<checkRank2.length; i++){
 		parseRank2.push(parseInt((checkRank2[i])))
 	}
 }
 function noZero(){
+
 	noZero1();
 	noZero2();
 }
 
 
-function LargestNum1(){
+function LargestNum1(){///Largest number from player 1 hand
 highCard1=0;
 
 for (i=0; i<=highCard1;i++){
@@ -347,7 +475,7 @@ for (i=0; i<=highCard1;i++){
 } return highCard1;
 }
 
-function LargestNum2(){
+function LargestNum2(){///Largest number from player 2 hand
 highCard2=0;
 
 	for (i=0;i<=highCard2;i++){
@@ -358,15 +486,105 @@ highCard2=0;
 	} return highCard2;
 }
 
-function winSingle(){
+function checkSingle(){///Determine high-card winner
+
+
 	if (LargestNum1()==LargestNum2()){
-		console.log("tie");
+
 	}if (LargestNum1()>LargestNum2()){
-        console.log("player 1 wins w"+" "+ LargestNum1())
+p1HandScores.push(1);
 } if (LargestNum2()>LargestNum1()){
-       console.log("player 2 wins w"+ " " + LargestNum2());  
+p2HandScores.push(1);
 }
 }
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+///Pairs///Trips//Fullhouse
 
 
-//Pair & Trips Function
+function pair1() {//frequency of numbers in hand 1
+	var counts1 = {},
+	    num;
+
+	for(var i = 0; i< parseRank1.length; i++) {
+	    num = parseRank1[i];
+	    counts1[num] = counts1[num] ? counts1[num]+1 : 1;
+	}
+	return counts1;
+}
+
+function pair2() {///^^^^
+	var counts2 = {},
+	    num;
+
+	for(var i = 0; i< parseRank2.length; i++) {
+	    num = parseRank2[i];
+	    counts2[num] = counts2[num] ? counts2[num]+1 : 1;
+
+	}
+	return counts2;
+}
+
+function findMax(theObj) {
+	var max = 0,
+        maxKey;
+
+	for(var k in theObj) {
+		if (theObj[k] > max) {
+			max = theObj[k];
+            maxKey = k;
+		}
+	}		
+	return maxKey;
+}
+
+function highestPair(){
+	var highPair1=findMax(pair1());
+	var highPair2=findMax(pair2());
+	if(highPair1>highPair2){
+		p1HandScores.push(2);
+		$('#Hands').append(highPair1);
+
+	}if(highPair2>highPair1){
+		p2HandScores.push(2);
+		$('#Hands').append(highPair2);
+	}
+}
+
+function checkMulti(){//evaluates for pairs,trips,twopairs,&fullhouses fckyea
+
+	counts1=pair1();
+	counts2=pair2();
+	var pairCount1=0;
+	var pairCount2=0;
+	for (i=0; i<=14;i++){
+		if (counts1[i]===2){
+        pairCount1++;
+        
+		} if (counts1[i]===3){
+		p1HandScores.push(4);
+		} if( pairCount1 >1 ) {
+        p1HandScores.push(3);
+		} if (counts1[i]===3 && pairCount1 ===1){
+		p1HandScores.push(7);
+		} if (counts1[i]===4){
+		p1HandScores.push(8);
+		}
+
+	} for (i=0; i<=14; i++){
+		if (counts2[i]===2){
+             pairCount2++;
+         
+		} if (counts2[i]===3){
+			p2HandScores.push(4)
+		} if (pairCount2 >1){
+			p2HandScores.push(3);
+		} if (counts2[i]===3 && pairCount2 ===1){
+			p2HandScores.push(7);
+		} if (counts1[i]===4){
+			p2HandScores.push(8);
+		}
+	}
+}	
+
+
